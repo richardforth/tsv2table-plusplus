57885166e688f4a97fb1af2bff8d05ad46bb6c40
Richard Forth <richard.forth@gmail.com>
2023-03-29 21:04:11 +0100

    Update tsv2table++.cpp
    Re-Written from the ground up

--- tsv2table++.cpp
+++ tsv2table++.cpp
@@ -17,122 +17,182 @@ using std::right;
 using std::setw;
 
 
+void make_table(vector <string> result, int maxcols, int maxrows, int maxcellsize);
+bool checkHasExactlyOneArg(int count);
+void process_stdin(char delimiter);
+void process_arg(string fname, char delimiter);
+
+bool checkHasExactlyOneArg(int count)
+{
+        //cout << "checkHasArgs() called." << endl;
+        //cout << "received variable: "  << count << endl;
+        if (count == 2) {
+                return true;
+        } else {
+                return false;
+        }
+}
+
+
+void process_arg(string fname, char delimiter)
+{
+        cout << "process_arg() called on " << fname << endl;
+
+        int cols = 0 ;  // Create counter variable for columns.
+        int rows = 0 ;  // Create counter variable for rows.
+        int maxcols = 0, maxrows = 0; // variables to keep highscores
+        int cellsize = 0 , maxcellsize = 0 ; // variables to dynamically size tables based on content
+        vector <string> result;
+        string line;
+        ifstream reader( fname ) ; // Create input file object from argument provided.
+        if( ! reader ) // Always check this.
+        {
+                cout << "Error opening input file." << endl ;
+                cout << "Reverting to STDIN..." << endl ;
+                process_stdin(delimiter);
+        }
+
+        while( ! reader.eof() ) // Loop through data...
+        {
+                cols = 0; // reset counter each new line
+                getline(reader, line);
+                rows++ ;
+                if (maxrows < rows)
+                        maxrows = rows ;
+                stringstream ss( line );
+                while( ss.good() )
+                {
+                        string substr;
+                        getline( ss, substr, delimiter ) ;
+
+                        cellsize = substr.size() ;
+                        if (maxcellsize < cellsize)
+                                maxcellsize = cellsize ;
+                        cols++ ;
+                        if (maxcols < cols)
+                                maxcols = cols ;
+                        if (substr != "" )
+                                result.push_back( substr ) ;
+                        else
+                                cout << "Empty line ignored." << endl ;
+                }
+        }
+        reader.close() ;
+        make_table(result, maxcols, maxrows, maxcellsize);
+}
+
+void process_stdin(char delimiter)
+{
+
+        int cols = 0 ;  // Create counter variable for columns.
+        int rows = 0 ;  // Create counter variable for rows.
+        int maxcols = 0, maxrows = 0; // variables to keep highscores
+        int cellsize = 0 , maxcellsize = 0 ; // variables to dynamically size tables based on content
+        cout << "process_stdin() called" << endl;
+        vector <string> result;
+        string line;
+
+        while( ! cin.eof() ) // Loop through data...
+        {
+                cols = 0; // reset counter each new line
+                getline(cin, line);
+                rows++ ;
+                if (maxrows < rows)
+                        maxrows = rows ;
+                stringstream ss( line );
+                while( ss.good() )
+                {
+                        string substr;
+                        getline( ss, substr, delimiter ) ;
+
+                        cellsize = substr.size() ;
+                        if (maxcellsize < cellsize)
+                                maxcellsize = cellsize ;
+                        cols++ ;
+                        if (maxcols < cols)
+                                maxcols = cols ;
+                        if (substr != "" )
+                                result.push_back( substr ) ;
+                        else
+                                cout << "Empty line ignored." << endl ;
+                }
+        }
+        make_table(result, maxcols, maxrows, maxcellsize);
+}
+
+
+void make_table(vector <string> result, int maxcols, int maxrows, int maxcellsize)
+{
+        cout << "make_table() called." << endl;
+        cout << "Result array size: " << result.size() << endl;
+        cout << "Max Cols: " << maxcols << endl;
+        cout << "Max Rows: " << maxrows << endl;
+        cout << "Max Cell Size: " << maxcellsize << endl;
+
+
+        int cols = 0 ;  // Reset and re-use counter variable for columns.
+        int rows = 0 ;  // Reset and re-use variable for rows.
+        int i = 0 ; // Add a new counter variable
+        while (i < result.size() )
+        {
+                if ( rows ==  0 )
+                {
+                        cout << right << setw(1) << "+" ;
+                        cols = 0;
+                        while ( cols < maxcols )
+                        {
+                                cout <<  setw(maxcellsize +2 ) << string((maxcellsize + 2), '-') << setw( 2 ) << "-+" ;
+                                cols++ ;
+                        }
+                        cout << endl ;
+
+                        cout << right << setw(1) << "|" ;
+                        cols = 0 ; // reset cols to 0
+                        while (cols < maxcols)
+                        {
+                                cout << right << setw(maxcellsize +2 ) << result.at( i++ ) << setw( 2 ) << " |" ;
+                                cols++ ;
+                        }
+                        cout << endl ;
+                        cols = 0 ; // reset cols to 0
+                        cout << right << setw(1) << "+" ;
+                        while ( cols < maxcols )
+                        {
+                                cout <<  setw(maxcellsize +2 ) << string((maxcellsize + 2), '-') << setw( 2 ) << "-+" ;
+                                cols++ ;
+                        }
+                        cout << endl ;
+                        rows++ ;
+                }
+                else
+                {
+                        cout << "|" ;
+                        cols = 0 ; // reset cols  for new row
+                        while (cols < maxcols)
+                        {
+                                cout << right << setw(maxcellsize +2 ) << result.at( i++ ) << setw( 2 ) << " |" ;
+                                cols++ ;
+                        }
+                        cout << endl ;
+                        rows++ ;
+                }
+        }
+        cols = 0 ; // reset cols to 0
+        cout << right << setw(1) << "+" ;
+        while ( cols < maxcols )
+        {
+                cout <<  setw(maxcellsize +2 ) << string((maxcellsize + 2), '-') << setw( 2 ) << "-+" ;
+                cols++ ;
+        }
+        cout << endl ;
+}
+
 int main(int argc, char *argv[])
 {
-    string line, fname ;
-    vector <string> result ; // Create a string array.
-    int cols = 0 ;  // Create counter variable for columns.
-    int rows = 0 ;  // Create counter variable for rows.
-    int maxcols = 0, maxrows = 0; // variables to keep highscores
-    int cellsize = 0 , maxcellsize = 0 ; // variables to dynamically size tables based on content
+      cout << "Main Function." << endl;
+      char delimiter = '\t';
+      cout << "Has Exactly One Argument? " << (checkHasExactlyOneArg(argc) ? "true" : "false") << endl;
+      checkHasExactlyOneArg(argc) ? process_arg(argv[1], delimiter) : process_stdin(delimiter);
 
-
-    // TODO: check for stdin
-    cout << "Argument Count: " << argc << endl;
-    cout << "Argument0: " << argv[0] << endl;
-    cout << "Argument1: " << argv[1] << endl;
-
-    if (argc != 2)
-    {
-        cout << "Usage: csv2table++ filename.csv" << endl ;
-        return -1 ;
-    }
-
-    fname = argv[1] ;
-    ifstream reader( fname ) ; // Create input file object from argument provided.
-    if( ! reader ) // Always check this.
-    {
-        cout << "Error opening input file" << endl ;
-        return -1 ;
-    }
-
-    while( ! reader.eof() ) // Loop through data...
-    {
-        cols = 0; // reset counter each new line
-        getline(reader, line);
-        rows++ ;
-        if (maxrows < rows)
-            maxrows = rows ;
-        stringstream ss( line );
-        while( ss.good() )
-        {
-            string substr;
-            getline( ss, substr, '\t' ) ;
-
-            cellsize = substr.size() ;
-            if (maxcellsize < cellsize)
-                maxcellsize = cellsize ;
-            cols++ ;
-            if (maxcols < cols)
-                maxcols = cols ;
-            if (substr != "" )
-                result.push_back( substr ) ;
-            else
-                cout << "Empty line ignored." << endl ;
-        }
-    }
-    reader.close() ;
-
-    cout << "Elements in result array: " << result.size() << endl ;
-    cout << "Max Columns: " << maxcols << endl ;
-    cout << "Max Rows: " << maxrows << endl ;
-    cout << "Max Cell Size: " << (maxcellsize + 2) << endl ; // +2 for one space margin each side
-
-
-    cols = 0 ;  // Reset and re-use counter variable for columns.
-    rows = 0 ;  // Reset and re-use variable for rows.
-    int i = 0 ; // Add a new counter variable
-    while (i < result.size() )
-    {
-        if ( rows ==  0 )
-        {
-            cout << right << setw(1) << "+" ;
-            cols = 0;
-            while ( cols < maxcols )
-            {
-                cout <<  setw(maxcellsize +2 ) << string((maxcellsize + 2), '-') << setw( 2 ) << "-+" ;
-                cols++ ;
-            }
-            cout << endl ;
-
-            cout << right << setw(1) << "|" ;
-            cols = 0 ; // reset cols to 0
-            while (cols < maxcols)
-            {
-                cout << right << setw(maxcellsize +2 ) << result.at( i++ ) << setw( 2 ) << " |" ;
-                cols++ ;
-            }
-            cout << endl ;
-            cols = 0 ; // reset cols to 0
-            cout << right << setw(1) << "+" ;
-            while ( cols < maxcols )
-            {
-                cout <<  setw(maxcellsize +2 ) << string((maxcellsize + 2), '-') << setw( 2 ) << "-+" ;
-                cols++ ;
-            }
-            cout << endl ;
-            rows++ ;
-        }
-        else
-        {
-            cout << "|" ;
-            cols = 0 ; // reset cols  for new row
-            while (cols < maxcols)
-            {
-                cout << right << setw(maxcellsize +2 ) << result.at( i++ ) << setw( 2 ) << " |" ;
-                cols++ ;
-            }
-            cout << endl ;
-            rows++ ;
-        }
-    }
-    cols = 0 ; // reset cols to 0
-    cout << right << setw(1) << "+" ;
-    while ( cols < maxcols )
-    {
-        cout <<  setw(maxcellsize +2 ) << string((maxcellsize + 2), '-') << setw( 2 ) << "-+" ;
-        cols++ ;
-    }
-    cout << endl ;
-    return 0 ;
+      return 0;
 }
